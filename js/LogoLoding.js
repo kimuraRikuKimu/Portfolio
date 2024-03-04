6d244c9b32b2252202fb9f0daf828961caa41576
kimuraRikuKimu <21j5141@stu.meisei-u.ac.jp>
2024-03-05 01:56:32 +0900

    BirdLogoAnnimationAdd

--- js/LogoLoding.js
+++ js/LogoLoding.js
@@ -3,7 +3,7 @@ var stroke;
 stroke = new Vivus('mask', {//アニメーションをするIDの指定
     start: 'manual',//自動再生をせずスタートをマニュアルに
     type: 'scenario-sync',// アニメーションのタイプを設定
-    duration: 10,//アニメーションの時間設定。数字が小さくなるほど速い
+    duration: 4,//アニメーションの時間設定。数字が小さくなるほど速い
     forceRender: false,//パスが更新された場合に再レンダリングさせない
     animTimingFunction: Vivus.EASE,//動きの加速減速設定
 },
@@ -13,7 +13,7 @@ stroke = new Vivus('mask', {//アニメーションをするIDの指定
 );
 
 $(window).on('load', function () {
-    $("#splash").delay(5000).fadeOut('slow');//ローディング画面を3秒（3000ms）待機してからフェイドアウト
-    $("#splash_logo").delay(5000).fadeOut('slow');//ロゴを3秒（3000ms）待機してからフェイドアウト
+    $("#splash").delay(5500).fadeOut('slow');//ローディング画面を3秒（3000ms）待機してからフェイドアウト
+    $("#splash_logo").delay(5500).fadeOut('slow');//ロゴを3秒（3000ms）待機してからフェイドアウト
     stroke.play();//SVGアニメーションの実行
 });
